448269e1c3924ba66a95c37d6eb44041dba5e73f
Abhijeet singh <singhrajputabhijeet0001@gmail.com>
2025-11-26 10:38:59 +0530

    chapter2
    this is the practie questions from chapter 2

--- p2.py
+++ p2.py
@@ -1,2 +1,3 @@
-import pyjokes
-print(pyjokes.get_joke())
+a=12
+b=5
+print("remainder:",a%b)
---
+++ p6.py
@@ -0,0 +1,2 @@
+a=int(input("Entre no 1: "))
+print("Square of this two no is",a**2) #use ** for exponential questuions
